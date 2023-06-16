d20565f3de26a735f84749656d62f0d2bab06e26
Crowdbotics <engineering@crowdbotics.com>
2023-06-16 12:23:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-feather-dev-92128",
+  url: "https://yellow-feather-dev-92128.botics.co",
+  api: "https://yellow-feather-dev-92128.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
